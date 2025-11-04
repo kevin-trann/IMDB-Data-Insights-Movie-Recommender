a116682edc659d29c4654a13b006a9adfbe211e7
kevin <trankevin789@gmail.com>
2025-11-03 21:28:25 -0500

    finished main features of data insight project into frontend
    implemented feature to recommend a title using algorithm into Streamlit

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 from fuzzywuzzy import process
 import pandas as pd
-import streamlit as st
 
 movies_and_tvShows = pd.read_csv("imdb_popular_dataset.csv")
 movies_and_tvShows = movies_and_tvShows[movies_and_tvShows['numVotes'] >= 50000]
@@ -11,98 +10,143 @@ tvShows = movies_and_tvShows[movies_and_tvShows['titleType'].isin(["tvSeries", "
 sortValue = ""
 mediaType = ""
 mediaName = ""
-ascendingOrder = False
 
-st.title("IMDB Data Insights & Movie Recommender by Kevin Tran")
 
-st.write("This project only contains titles from 1980-2025 that have more than 50,000 votes on IMDB.")
+intro = True
+askUserInput = True
 
-askUserInput = st.selectbox("What would you like to do?", ["", "List top movies/tv shows in a chosen category", "Recommend a title based on input", "Info about a title"])
-
-if askUserInput == "List top movies/tv shows in a chosen category":
-    
-    askMediaType = st.selectbox("What type of media would you like to sort?", ["Movies", "Tv Shows", "Both"])
-    
-    if askMediaType == "Movies":
-        mediaType = movies
-        mediaName = "Movies"
-      
-    elif askMediaType == "Tv Shows":
-        mediaType = tvShows
-        mediaName = "Tv Shows"
+while True:
+    if (intro):
+        print("---IMDB Data Insights & Movie Recommender by Kevin Tran---\n")
+        intro = False
+        
+    if (askUserInput):
+        value = input("\nWhat would you like to do?\n\n1.List top movies/tv shows in a chosen category\n2.Recommend a title based on input\n3.Info about a title\n\nPlease type in the corresponding number for the option chosen.")
+        askUserInput = False
        
-    elif askMediaType == "Both":
-        mediaType = movies_and_tvShows
-        mediaName = "Movies and Tv Shows"
-     
-        
-    sortingMethod = st.selectbox(f"How would you like to sort the titles by?", ["Rating", "Number of Votes", "Year of Release", "Alphabetical Order"])
-    
-    if sortingMethod == "Rating":
-        sortValue = "averageRating"
-        
-    elif sortingMethod == "Number of Votes":
-        sortValue = "numVotes"
-    
-    elif sortingMethod == "Year of Release":
-        sortValue = "startYear"
-        
-    elif sortingMethod == "Alphabetical Order":
-        sortValue = "primaryTitle"
-        
-    numberOfEntries = st.number_input("How many titles would you like to list? Please type an integer.",
-    min_value=1,
-    max_value=100,
-    value=5,
-    step=1 
-    )
-    
-    if sortingMethod == "Rating" or sortingMethod == "Number of Votes":
-    
-        rankingOrder = st.selectbox("What order would you like to display the data?", ["Highest to Lowest", "Lowest to Highest"])
-        
-        if rankingOrder == "Highest to Lowest":
-            ascendingOrder = False
-        
-        elif rankingOrder == "Lowest to Highest":
-            ascendingOrder = True
-    
-    if sortingMethod == "Year of Release":
-        
-        rankingOrder = st.selectbox("What order would you like to display the data?", ["Chronological Order", "Reverse Chronological Order"])
-        
-        if rankingOrder == "Chronological Order":
-            ascendingOrder = True
-            
-        elif rankingOrder == "Reverse Chronological Order":
-            ascendingOrder = False
-    
-    if sortingMethod == "Alphabetical Order":
-        
-        rankingOrder = st.selectbox("What order would you like to display the data?", ["A-Z", "Z-A"])
-        
-        if rankingOrder == "A-Z":
-            ascendingOrder = True
-        
-        elif rankingOrder == "Z-A":
-            ascendingOrder = False
-    
-    if st.button("Run"):
-        topList = mediaType.sort_values(by=sortValue, ascending=ascendingOrder).head(int(numberOfEntries))
-        topList = (topList[["primaryTitle", "averageRating", "numVotes", "startYear"]].reset_index(drop=True))
-        topList.index = topList.index + 1
-        st.header(f"Top {mediaName} by {sortingMethod}")
-        st.dataframe(topList)
-   
-        
-        
-elif askUserInput == "Recommend a title based on input":
-    st.write("2")
-    
-elif askUserInput == "Info about a title":
-    titles = movies_and_tvShows['primaryTitle']
-    search = st.text_input("Which title would you like to search up info for?")
-    if st.button("Search"):
-        bestMatch = process.extractOne(search, titles)
-        movieId = bestMatch[0]
-        st.dataframe(movies_and_tvShows[movies_and_tvShows['primaryTitle'] == movieId][["primaryTitle", "averageRating", "numVotes", "startYear"]])
+        match value:
+            case "1":
+                
+                while True: 
+                    mediaType = input("\nWhat media type would you like to sort by?\n1. Movies\n2. Tv Shows\n3. Both\n\nPlease type in the corresponding number for the option chosen.")
+                    
+                    match mediaType:
+                        
+                        case "1": 
+                            mediaType = movies
+                            mediaName = "Movies"
+                            break
+                        case "2":
+                            mediaType = tvShows
+                            mediaName = "Tv Shows"
+                            break
+                        case "3":
+                            mediaType = movies_and_tvShows
+                            mediaName = "Movies and Tv Shows"
+                            break
+                        case _:
+                            print("Invalid input. Please try again.")
+                        
+                while True:
+                    categoryRanking = input("\nHow would you like to sort the top 10 movies/tv shows?\n1. Rating\n2. Number of voters\n\nPlease type in the corresponding number for the option chosen.")
+                    
+                    match categoryRanking:
+                        
+                            case "1":
+                                sortValue = "averageRating"
+                                break
+                            
+                            case "2":
+                                sortValue = "numVotes"
+                                break
+                            case _:
+                                print("Invalid input. Please try again.")
+                
+                while True:
+                    numberOfEntries = input(f"\nHow many {mediaName} would you like to list?\nPlease type in an integer.")
+                    
+                    try: 
+                        numberOfEntries = int(numberOfEntries)
+                        break   
+                    except ValueError: 
+                        print("\n---Invalid input, please enter an integer.---")            
+
+                topList = mediaType.sort_values(by=sortValue, ascending=False).head(int(numberOfEntries))
+                print("")
+                print(f"---Top {numberOfEntries} {mediaName} by {sortValue}---")
+                print(topList[["primaryTitle", "averageRating", "numVotes", "startYear"]].to_string(index=False))
+                cont = input("\nPress any key to continue\n")
+                if (cont):
+                    askUserInput = True
+                
+            case "2":
+                while True:
+                    reference = input("\nWhat movie or TV Show do you want recommendations based on?")
+                    titles = movies_and_tvShows['primaryTitle']
+                    bestMatch = process.extractOne(reference, titles)
+                    
+                    if bestMatch:
+                        movieId = bestMatch[0]
+                        chosenDataset = movies_and_tvShows['primaryTitle'] == movieId
+                        chosenTitle = (movies_and_tvShows[chosenDataset]["primaryTitle"].values[0])
+                        print("")
+                        print(f"Your chosen movie is {chosenTitle}")
+                        
+                        chosenYear = (movies_and_tvShows[chosenDataset]["startYear"].values[0])
+                        minYear = int(chosenYear) - 10
+                        maxYear = int(chosenYear) + 10
+                        
+                        chosenGenres = (movies_and_tvShows[chosenDataset]['genres'].values[0])
+                        chosenGenresList = chosenGenres.split(",")
+                        
+                        while True:
+                            numberOfEntries = input(f"\nHow many recommendations would you like to be recommended?\nPlease type in an integer.")
+                            
+                            try: 
+                                numberOfEntries = int(numberOfEntries)
+                                break   
+                            except ValueError: 
+                                print("\n---Invalid input, please enter an integer.---")   
+                                
+                        recommendations = movies_and_tvShows.copy()[
+                        (movies_and_tvShows['startYear'] >= minYear) 
+                        & (movies_and_tvShows['startYear'] <= maxYear)
+                        & (movies_and_tvShows['primaryTitle'] != chosenTitle)]
+                        
+                        for genre in chosenGenresList:
+                            recommendations = recommendations[recommendations['genres'].str.contains(genre, na=False)]
+                        
+                        printRecommendations = recommendations.sort_values(by="averageRating", ascending=False).head(int(numberOfEntries))
+                        print(printRecommendations[["primaryTitle", "averageRating", "numVotes", "startYear"]].to_string(index=False))
+                        
+                        cont = input("\nPress any key to continue\n")
+                        if (cont):
+                            askUserInput = True
+                            break
+                        
+                    else:
+                        print("\nNo results. Please try again.")
+                    
+            case "3":
+                
+                while True:
+                    search = input("\nWhich movie/tv show would you like to search up info for?")
+                    titles = movies_and_tvShows['primaryTitle']
+                    bestMatch = process.extractOne(search, titles)
+                    
+                    if bestMatch:
+                        movieId = bestMatch[0]
+                        print("")
+                        print(movies_and_tvShows[movies_and_tvShows['primaryTitle'] == movieId][["primaryTitle", "averageRating", "numVotes", "startYear"]])
+                        
+                        cont = input("\nPress any key to continue\n")
+                        if (cont):
+                            askUserInput = True
+                            break
+                    
+                    else:
+                        print("\nNo results. Please try again.")
+                        
+                    
+                    
+                    
